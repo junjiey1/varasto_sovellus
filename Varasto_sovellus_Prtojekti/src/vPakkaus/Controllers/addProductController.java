e1739d2d46c9216978a807186e2e8d3d29796194
Grigorij Semykin <Grigorij Semykin@Grigorij.wlan.metropolia.fi>
2016-09-15 10:41:44 +0300

    addProductController

---
+++ Varasto_sovellus_Prtojekti/src/vPakkaus/Controllers/addProductController.java
@@ -0,0 +1,35 @@
+package vPakkaus.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class addProductController {
+
+    @FXML
+    private TextField productName;
+    @FXML
+    private TextField quantity;
+    @FXML
+    private TextField price;
+    @FXML
+    private TextField weight;
+    @FXML
+    private TextField volume;
+    @FXML
+    private TextField whLocation;
+    
+    
+    boolean allGood;
+    
+    
+    
+    public void addNewProduct(){
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    }
+}

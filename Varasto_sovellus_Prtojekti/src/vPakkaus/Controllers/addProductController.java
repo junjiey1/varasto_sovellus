20b3b3e2b2cc1a36ee9124b2f3d1ef5a1e743037
julius <julius@julius-Aspire-E5-521G>
2016-10-04 13:26:10 +0300

    fix

--- Varasto_sovellus_Prtojekti/src/vPakkaus/Controllers/addProductController.java
+++ Varasto_sovellus_Prtojekti/src/vPakkaus/Controllers/addProductController.java
@@ -1,5 +1,4 @@
 package vPakkaus.Controllers;
-
 import java.awt.datatransfer.Clipboard;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,9 +14,7 @@ import javafx.scene.input.Dragboard;
 import vPakkaus.CurrentDate;
 import vPakkaus.DB_AccessObject;
 import vPakkaus.Product;
-
 public class addProductController {
-
 	@FXML
 	private TextField productName;
 	@FXML
@@ -30,27 +27,24 @@ public class addProductController {
 	private TextField volume;
 	@FXML
 	private TextField whLocation;
-
 	private MainController mc;
 	boolean allGood;
-
 	File file;
 	Scanner input;
 	String[] oneRowOfData;
-	String path, pName;
-
+	String path, pName, pShelf, clientName, clientAddress;
+	double pWeight, pVolume;
+	int pQuantity;
+	float pPrice;
 	public void setMainController(MainController m) {
 		mc = m;
 	}
-
 	public void AddProductManually() throws IOException {
 		allGood = true;
-
 		if (productName.getText().isEmpty() || quantity.getText().isEmpty() || price.getText().isEmpty()
 				|| weight.getText().isEmpty() || volume.getText().isEmpty() || whLocation.getText().isEmpty()) {
 			allGood = false;
 		}
-
 		try {
 			Integer.parseInt(quantity.getText());
 			Double.parseDouble(volume.getText());
@@ -59,30 +53,24 @@ public class addProductController {
 		} catch (NumberFormatException ex) {
 			allGood = false;
 		}
-
 		if (allGood) {
-
 			int lisaajan_id = mc.getID();
-
 			Product product = new Product(productName.getText(), whLocation.getText(),
 					Double.parseDouble(weight.getText()), Double.parseDouble(volume.getText()),
 					Float.parseFloat(price.getText()));
-
 			mc.AddProduct(product.getProduct_name(), product.getProduct_weight(), product.getProduct_volume(),
 					product.getProduct_location(), product.getProduct_price(), Integer.parseInt(quantity.getText()));
-
+			
 		} else {
 			System.out.println("joku kenttä on tyhjä tai väärin täytetty");
 		}
 	}
-
 	// public boolean addNewProduct(Product product){
 	// Lisaa(product.getProduct_name(), product.getProduct_weight(),
 	// product.getProduct_volume(), product.getProduct_location(),
 	// product.getProduct_price());
 	//
 	// }
-
 	@FXML
 	public void handleFilesDragDropped(DragEvent event) throws FileNotFoundException {
 		Dragboard db = event.getDragboard();
@@ -103,9 +91,8 @@ public class addProductController {
 					}
 					readFromFile(oneRowOfData[i]);
 				}
-
 			// ONLY 1 FILE DRAGGED
-			} else {
+			} else { 
 				path = path.substring(1, path.length()-1);
 				readFromFile(path);
 			}
@@ -114,17 +101,28 @@ public class addProductController {
 		event.consume();
 		db = null;
 	}
-
 	public void readFromFile(String name) throws FileNotFoundException {
-
 		file = new File(name);
+		//SSKANKDKFAQBFBQAFOB
 		input = new Scanner(file);
-
 		while (input.hasNext()) {
-			// oneRowOfData = input.nextLine().split(",");
-			System.out.println(input.nextLine());
+			oneRowOfData = input.nextLine().split(",");
+			if (oneRowOfData.length == 2){
+				clientName = oneRowOfData[0];
+				clientAddress = oneRowOfData[1];
+			}else{
+				pName = oneRowOfData[0];
+				pWeight = Double.parseDouble(oneRowOfData[1]);
+				pVolume = Double.parseDouble(oneRowOfData[2]);
+				pShelf = oneRowOfData[3];
+				pPrice = Float.parseFloat(oneRowOfData[4]);
+				pQuantity = Integer.parseInt(oneRowOfData[5]);
+				
+				mc.AddProduct(pName, pWeight, pVolume, pShelf, pPrice, pQuantity);
+				
+			}
+			System.out.println(clientName+" "+clientAddress+" "+pName+" "+pWeight+" "+pVolume+" "+pShelf+" "+pPrice+" "+pQuantity);
 		}
 		input.close();
 	}
-
 }

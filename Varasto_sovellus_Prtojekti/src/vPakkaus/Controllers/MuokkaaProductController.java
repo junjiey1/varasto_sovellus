7652c579d99a4aaeb374db0c438024ec9371ea6a
Julius <Julius@Julle.wlan.metropolia.fi>
2016-10-06 15:32:52 +0300

    test

--- Varasto_sovellus_Prtojekti/src/vPakkaus/Controllers/MuokkaaProductController.java
+++ Varasto_sovellus_Prtojekti/src/vPakkaus/Controllers/MuokkaaProductController.java
@@ -3,16 +3,21 @@ package vPakkaus.Controllers;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.Callback;
 import vPakkaus.Product;
 
 public class MuokkaaProductController implements SetMainController{
@@ -37,6 +42,7 @@ public class MuokkaaProductController implements SetMainController{
 
 	@FXML
 	private TableColumn<Product,Double> volumeCol;
+	private Callback<TableColumn, TableCell> cellFactory;
 
 
 	private MainController mc;
@@ -45,12 +51,49 @@ public class MuokkaaProductController implements SetMainController{
 	ObservableList<Product> tuote = FXCollections.observableArrayList();
 	boolean hae;
 
+
+
+
+
+//	private class RowSelectChangeListener implements ChangeListener {
+//			        @Override
+//			        public void changed(ObservableValue ov,Number oldVal, Number newVal) {
+//			            int ix = newVal.intValue();
+//			            if ((ix = tuote.size())) {
+//			                return; // invalid data
+//			            }
+//			            Product p = tuote.get(ix);
+//			            actionStatus.setText(p.toString());
+//			        }
+//
+//					@Override
+//					public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+//						// TODO Auto-generated method stub
+//
+//					}
+//			    }
+
+
+
+
 	public void initialize(){
-		System.out.println("menee");
-		idCol = new TableColumn<Product, Integer>("ID");
+		tuoteTable = new TableView<>();
 		tuoteTable.setEditable(true);
-		idCol.setEditable(true);
-		nameCol.setEditable(true);
+		System.out.println("menee");
+
+		idCol = new TableColumn<Product, Integer>("id");
+		nameCol = new TableColumn<Product, String>("nimi");
+		weightCol = new TableColumn<Product, Double>("weight");
+		priceCol = new TableColumn<Product, Float>("hinta");
+		volumeCol = new TableColumn<Product, Double>("tilavuus");
+
+		idCol.setCellValueFactory(new PropertyValueFactory<>("ID"));
+		nameCol.setCellValueFactory(new PropertyValueFactory<Product,String>("product_name"));
+		weightCol.setCellValueFactory(new PropertyValueFactory<Product, Double>("product_weight"));
+		priceCol.setCellValueFactory(new PropertyValueFactory<Product,Float>("product_price"));
+		volumeCol.setCellValueFactory(new PropertyValueFactory<Product,Double>("product_volume"));
+
+		//tuoteTable.getSelectionModel().selectedIndexProperty().addListener(new RowSelectChangeListener());
 	}
 
 	public void SearchManually()throws IOException {
@@ -65,22 +108,43 @@ public class MuokkaaProductController implements SetMainController{
 				return; //error viesti tänne ku ei löytynyt mitään
 			try{
 				for(Product pro : p){
-					idCol.setCellValueFactory(new PropertyValueFactory<>("ID"));
+					tuote.add(pro);
+					System.out.println(pro.getID());
+//					idCol.setCellValueFactory(new PropertyValueFactory<>("ID"));
+//					nameCol.setCellValueFactory(new PropertyValueFactory<Product,String>("product_name"));
+//					weightCol.setCellValueFactory(new PropertyValueFactory<Product, Double>("product_weight"));
+//					priceCol.setCellValueFactory(new PropertyValueFactory<Product,Float>("product_price"));
+//					volumeCol.setCellValueFactory(new PropertyValueFactory<Product,Double>("product_volume"));
+//					idCol.setCellValueFactory(new PropertyValueFactory<>("ID"));
+//					idCol.setEditable(true);
+					//idCol.setCellFactory(cellFactory);
 					//idCol.setCellFactory(TextFieldTableCell.<Product>forTableColumn());
-//					idCol.setOnEditCommit(
+
+//					idCol.setOnEditStart(
 //			            (CellEditEvent<Product, Integer> t) -> {
 //			                ((Product) t.getTableView().getItems().get(
 //			                        t.getTablePosition().getRow())
-//			                        ).setID(t.getNewValue());
+//			                        ).setID(1);
 //			        });
-					nameCol.setCellValueFactory(new PropertyValueFactory<Product,String>("product_name"));
-					weightCol.setCellValueFactory(new PropertyValueFactory<Product, Double>("product_weight"));
-					priceCol.setCellValueFactory(new PropertyValueFactory<Product,Float>("product_price"));
-					volumeCol.setCellValueFactory(new PropertyValueFactory<Product,Double>("product_volume"));
-					tuoteTable.getItems().add(pro);
-				}
-			}catch(Exception e){
+//					idCol.setCellValueFactory(cellData -> {
+//			            Product rowIndex = cellData.getValue();
+//			            return new ReadOnlyIntegerWrapper(pro.getID());
+//			        });
+//					nameCol.setCellValueFactory(new PropertyValueFactory<Product,String>("product_name"));
+//					weightCol.setCellValueFactory(new PropertyValueFactory<Product, Double>("product_weight"));
+//					priceCol.setCellValueFactory(new PropertyValueFactory<Product,Float>("product_price"));
+//					volumeCol.setCellValueFactory(new PropertyValueFactory<Product,Double>("product_volume"));
+					//tuoteTable.getItems().add(pro);
 
+				}
+
+				for(Product p : tuote){
+					System.out.println("-"+p.getID());
+				}
+				tuoteTable.setItems(tuote);
+				//tuoteTable.getColumns().addAll(idCol, nameCol, weightCol, priceCol, volumeCol);
+			}catch(Exception e){
+				System.out.println("lol");
 			}
 
 		}else {

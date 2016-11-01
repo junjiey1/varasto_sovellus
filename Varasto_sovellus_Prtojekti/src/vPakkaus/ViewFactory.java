92f61d4f8cf237faf6c7efd8e6d0ecafea4e22da
Julius <Julius@Julle.wlan.metropolia.fi>
2016-11-01 15:00:36 +0200

    help

---
+++ Varasto_sovellus_Prtojekti/src/vPakkaus/ViewFactory.java
@@ -0,0 +1,22 @@
+package vPakkaus;
+
+import vPakkaus.Controllers.SetMainController;
+
+public class ViewFactory implements ViewFactory_IF{
+
+	@Override
+	public SetMainController annaLogin() {
+		return null;
+	}
+
+	@Override
+	public SetMainController annaSearch() {
+		return null;
+	}
+
+	@Override
+	public SetMainController annaMain() {
+		return null;
+	}
+
+}
---
+++ Varasto_sovellus_Prtojekti/src/vPakkaus/ViewFactory_IF.java
@@ -0,0 +1,8 @@
+package vPakkaus;
+import vPakkaus.Controllers.SetMainController;
+
+public interface ViewFactory_IF {
+	public SetMainController annaLogin();
+	public SetMainController annaSearch();
+	public SetMainController annaMain();
+}

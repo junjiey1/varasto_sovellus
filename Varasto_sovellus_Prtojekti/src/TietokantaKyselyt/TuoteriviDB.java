643e1f787d1de302048952e1cdddd9f68119334d
Teemu <Teemu@LAPTOP-CIPIFBFM.wlan.metropolia.fi>
2016-11-17 12:12:06 +0200

    local

---
+++ Varasto_sovellus_Prtojekti/src/TietokantaKyselyt/TuoteriviDB.java
@@ -0,0 +1,137 @@
+package TietokantaKyselyt;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import vPakkaus.Tuotejoukko;
+
+public class TuoteriviDB {
+
+	private Connection conn = null;
+	private PreparedStatement ps = null;
+	private ResultSet rs = null;
+	private ProductDB productdb = null;
+	private HyllyDB hyllydb = null;
+
+	public TuoteriviDB(Connection conn, ProductDB productdb, HyllyDB hyllydb) {
+		this.conn = conn;
+		this.hyllydb = hyllydb;
+		this.productdb = productdb;
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArrayList<Tuotejoukko> haeHyllynTuotejoukot(String hyllynimi) {
+
+		ArrayList<Tuotejoukko> tj = new ArrayList();
+		ArrayList<String> nimet = new ArrayList();
+		ArrayList<Integer> määrät = new ArrayList();
+
+		try {
+			ps = conn.prepareStatement(
+					"SELECT tuote.nimi, tuoterivi.maara FROM tuote, tuoterivi WHERE tuote.tuoteID = tuoterivi.tuoteID AND tuoterivi.hyllypaikka = ?;");
+
+			// Asetetaan argumentit sql-kyselyyn
+			ps.setString(1, hyllynimi);
+			rs = ps.executeQuery();// Hae annetulla käyttäjänimellä
+			// tietokanta rivi
+
+			while (rs.next()) {
+				int maara = rs.getInt("maara");
+				String nimi = rs.getString("nimi");
+				nimet.add(nimi);
+				määrät.add(maara);
+			}
+
+			for (int i = 0; i < nimet.size(); i++) {
+				Tuotejoukko tuotejoukko = new Tuotejoukko(productdb.findProduct(nimet.get(i)), hyllydb.HaeHylly(hyllynimi),
+						määrät.get(i));
+				tj.add(tuotejoukko);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return tj;
+	}
+
+	public boolean addProductToTuoteriviTable(Tuotejoukko joukko) {
+
+		try {
+			ps = conn.prepareStatement("INSERT INTO tuoterivi(tuoteID, hyllypaikka, maara)" + "VALUES (?,?,?);");
+
+			ps.setInt(1, joukko.getProduct().getID());
+			ps.setString(2, joukko.getHylly().getNimi());
+			ps.setInt(3, joukko.getMaara());
+
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			System.out.println("Lisäys epäonnistui tuoterivitaulukkoon!");
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public int tuotteidenMaaraHyllyssa(String nimi, String hyllypaikka) {
+		int maara = 0;
+		try {
+			ps = conn.prepareStatement(
+					"Select tuoterivi.maara FROM tuoterivi, tuote WHERE tuoterivi.tuoteID = tuote.tuoteID AND tuote.nimi = ? and  tuoterivi.hyllypaikka = ?;");
+			ps.setString(1, nimi);
+			ps.setString(2, hyllypaikka);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				maara = rs.getInt("maara");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ps.close();
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return maara;
+
+	}
+
+	public boolean MuokkaaTuoteriviä(Tuotejoukko tuotejoukko) {
+		boolean error = false;
+
+		try {
+			ps = conn.prepareStatement(
+					"UPDATE tuote, tuoterivi SET tuoterivi.maara = ?  WHERE tuote.tuoteID = tuoterivi.tuoteID AND tuote.nimi = ? AND tuoterivi.hyllypaikka = ?;");
+
+			ps.setInt(1, tuotejoukko.getMaara());
+			ps.setString(2, tuotejoukko.getProduct().getProduct_name());
+			ps.setString(3, tuotejoukko.getHylly().getNimi());
+
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			error = true;
+		}
+
+		return error;
+	}
+
+}
---
+++ Varasto_sovellus_Prtojekti/src/TietokantaKyselyt/lampotilaDB.java
@@ -0,0 +1,165 @@
+package TietokantaKyselyt;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import vPakkaus.Product;
+import vPakkaus.Tuotejoukko;
+
+public class lampotilaDB {
+	private Connection conn = null;
+	private PreparedStatement ps = null;
+	private ResultSet rs = null;
+
+	public lampotilaDB(Connection conn) {
+		this.conn = conn;
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean addTemperatures(Product product) {
+		// Tarvitaan tuotteen ID jotta voidaan lisätä arvoja Lämpötila
+		// taulukkoon
+		System.out.println("ID " + product.getID());
+		try {
+			ps = conn.prepareStatement("INSERT INTO lampotila(tuoteID, lampotila_max, lampotila_min) VALUES (?,?,?);");
+			// haetaan tuotteen id tietokannasta
+
+			ps.setInt(1, product.getID());
+			ps.setInt(2, product.getMax_temperature());
+			ps.setInt(3, product.getMin_temperature());
+
+			ps.executeUpdate();
+			ps.close();
+
+		} catch (SQLException e) {
+			System.out.println("Lisäys epäonnistui lampotilataulukkoon!");
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public boolean checkLampotila(Tuotejoukko joukko) {
+		boolean lampotila = true;
+		if (joukko.getProduct().getMax_temperature() != null & joukko.getProduct().getMin_temperature() != null) {
+			if (joukko.getHylly().getLämpötila() <= joukko.getProduct().getMax_temperature()
+					&& joukko.getHylly().getLämpötila() >= joukko.getProduct().getMin_temperature()) {
+				System.out.println("Tuotteen lämpötila on sopiva hyllypaikkaan");
+			} else {
+				System.out.println("Tuotteen lämpötila vaatimus ei vastaa hyllyn lämpötilaa");
+				lampotila = false;
+			}
+		} else {
+			System.out.println("tuotteelle ei ole asetettu lämpötila rajoituksia");
+		}
+		return lampotila;
+	}
+
+	public Product findTemperatures(Product pro) {
+		if (pro.getTemp()) {
+			try {
+				ps = conn.prepareStatement("SELECT lampotila_max, lampotila_min FROM lampotila WHERE tuoteID = ?");
+				// Asetetaan argumentit sql-kyselyyn
+				ps.setInt(1, pro.getID());
+				rs = ps.executeQuery();
+				while (rs.next()) {
+					int lampotila_max = rs.getInt("lampotila_max");
+					int lampotila_min = rs.getInt("lampotila_min");
+					pro.setMax_temperature(lampotila_max);
+					pro.setMin_temperature(lampotila_min);
+				}
+			} catch (SQLException e) {
+				System.out.println("Ei voitu hakea lämpötilaa!");
+				e.printStackTrace();
+			} finally {
+				try {
+					ps.close();
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		return pro;
+	}
+
+	public boolean checkIfTuoteIDExcistInLampoTila(int ID) {
+		int res = 0;
+		try {
+			ps = conn.prepareStatement("SELECT 1 AS total FROM lampotila WHERE tuoteID = ?;");
+			ps.setInt(1, ID);
+			rs = ps.executeQuery();
+			while (rs.next())
+				res = rs.getInt("total");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} finally {
+			try {
+				ps.close();
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("RES " + res);
+		if (res == 1)
+			return true;
+		return false;
+	}
+
+	public boolean deleteLampotila(Product p) {
+		try {
+			ps = conn.prepareStatement("UPDATE tuote SET lampotila_boolean = ? WHERE tuoteID = ?");
+			ps.setInt(1, 0);
+			ps.setInt(2, p.getID());
+			ps.executeUpdate();
+			ps.close();
+			ps = conn.prepareStatement("DELETE FROM lampotila WHERE tuoteID = ?");
+			ps.setInt(1, p.getID());
+			ps.executeUpdate();
+			ps.close();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean updateLampotila(Product p) {
+		try {
+			if (!checkIfTuoteIDExcistInLampoTila(p.getID())) {
+				System.out.println("set");
+				ps = conn.prepareStatement("UPDATE tuote SET lampotila_boolean = ? WHERE tuoteID = ?");
+				ps.setInt(1, 1);
+				ps.setInt(2, p.getID());
+				ps.executeUpdate();
+				ps.close();
+				ps = conn.prepareStatement(
+						"INSERT INTO lampotila (lampotila_max, lampotila_min, tuoteID) VALUES (?,?,?)");
+				ps.setInt(1, p.getMax_temperature());
+				ps.setInt(2, p.getMin_temperature());
+				ps.setInt(3, p.getID());
+				ps.executeUpdate();
+			} else {
+				System.out.println("upd");
+				ps = conn.prepareStatement(
+						"UPDATE lampotila SET lampotila_max = ?, lampotila_min = ? WHERE tuoteID = ?");
+				ps.setInt(1, p.getMax_temperature());
+				ps.setInt(2, p.getMin_temperature());
+				ps.setInt(3, p.getID());
+				ps.executeUpdate();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+}
